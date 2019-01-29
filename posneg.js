390d1c22757096f752bb7b7376bd29e7888baed6
darshanjag <itsdarshanjagtap@gmail.com>
2019-01-29 14:22:44 +0530

    pos neg number

---
+++ posneg.js
@@ -0,0 +1,8 @@
+//program to determine wehter the number is positive or negative
+var input=-1
+if (input>0){
+	console.log("the given number is positive");
+}
+else{
+	console.log("the given number is negative");
+}
