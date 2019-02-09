e9dab5435ac9902c078dcd98de76873d733350db
darshanjag <itsdarshanjagtap@gmail.com>
2019-02-09 21:51:10 +0530

    object

---
+++ objec.js
@@ -0,0 +1,16 @@
+var person= {
+	firstname: 'darshan',
+	lastname: 'jagtap',
+	age: 23,
+	address: {
+		street: '555 something st',
+		city: 'nasik',
+		state: 'mh'
+
+	}
+
+
+
+
+}
+console.log(person.address.state);
