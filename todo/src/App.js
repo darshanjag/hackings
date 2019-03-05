9ccade0503caab5f366d2a45318e245616b85b6b
darshanjag <itsdarshanjagtap@gmail.com>
2019-03-05 06:10:34 +0530

    react program

---
+++ todo/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import './App.css'
+class App extends Component {
+  state={
+    todos: [
+      {
+        id:1,
+        title : 'take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title : 'dinner with family',
+        completed: false
+      },
+      {
+        id:3,
+        title : 'meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    return (
+      
+      <div className="App">
+       <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+
+export default App;
