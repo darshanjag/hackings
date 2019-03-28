6a75fa2b69c83df24195dcd21b2c51194a4c4dbe
darshanjag <itsdarshanjagtap@gmail.com>
2019-03-28 11:00:22 +0530

    adding array elements

---
+++ min_value_arr.js
@@ -0,0 +1,13 @@
+/*Note : Solutions should be uploaded to GitHub by EODProblem
+ - 1:Statement:Input->Arrayof'n'elements.Update all elements
+ of givenarray to some MINIMUM value,such that SUM of all element
+  so this new array is strictly greater than the SUM of all elements of initial array.
+  */
+arr1 =[1,2,3,4,5];
+var sum = 0;
+for(let i=0; i<arr1.length;i++){
+	sum=sum+arr1[i];
+	console.log(sum);
+	
+}
+console.log(sum);
