206e4062e05620b11c7c03757f241e52c3fbfa54
darshanjag <itsdarshanjagtap@gmail.com>
2019-03-31 00:00:32 +0530

    adding a component

---
+++ react_code_camp/src/components/Myinfo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function Myinfo(){
+    return(
+        <div>
+        <h1>darshan</h1>
+        <p>am darshan aim to be a web dev</p>
+        <ul>
+        <li>paris</li>
+        <li>vegas</li>
+        <li>Venice</li>
+        </ul>
+        </div>
+    )
+}
+export default Myinfo
+
+
+
+
