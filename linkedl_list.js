c3c1592e99a1867c8f7f29f0b3925fa4f3ca3033
darshanjag <itsdarshanjagtap@gmail.com>
2019-04-01 23:38:21 +0530

    liniked list in js

---
+++ linkedl_list.js
@@ -0,0 +1,33 @@
+function LinkedList() {
+    this.head = null;
+    this.tail = null;    
+}
+
+function Node(prev, value, next) {
+
+    this.prev = prev;
+    this.value = value;
+    this.next = next;
+
+}
+
+LinkedList.prototype.addToHead = function(value) {
+
+    let newNode = new Node(null, value, this.head);
+
+    if(this.head){
+        this.head.prev = newNode;
+    }
+    else{
+        this.tail = newNode;
+    }
+
+    this.head = newNode;
+
+};
+var l =new LinkedList();
+l.addToHead(5)
+l.addToHead(6)
+l.addToHead(7)
+l.addToHead(8)
+console.log(l);
