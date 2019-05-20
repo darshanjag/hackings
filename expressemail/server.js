d0fa94f640c582652ef1d9eb26481d6f942b4b8b
darshanjag <itsdarshanjagtap@gmail.com>
2019-05-20 13:29:23 +0530

    added form

--- expressemail/server.js
+++ expressemail/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 var app = express();
 var Email= "itsdarshanjagtap@gmail.com"
 const port = process.env.PORT || 8080;
-var db = require('./dbconnect');
 var User = require('./models/user');
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
@@ -12,20 +11,14 @@ app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.post('/',function(req,res){
- var user = new User();
-
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.phone = req.body.phone;
-    user.problem=req.body.problem;
-    user.recieptn= req.body.recieptn;
+ 
     let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
       user: Email, // generated ethereal user
-      pass: your password // generated ethereal password
+      pass: "Rachitblossome6258"// generated ethereal password
     }
   });
  let mailOptions = {
@@ -33,8 +26,8 @@ app.post('/',function(req,res){
     to: "itsdarshanjagtap@gmail.com, "+req.body.email,  // list of receivers
     subject: "complaint", // Subject line
     text: "hello darshan", // plain text body
-    html: `<b>New Complaint</b><br><h3>name:` + req.body.name+`<br>phone:`+req.body.phone+`<br>email:`+req.body.email+
-    `<br>problem:`+req.body.problem+`<br>recieptn:`+req.body.recieptn + `</h3>`// html body
+    html: `<b>New Complaint</b><br><h3>name:` + req.body.fname+`<br>phone:`+req.body.lname+`<br>email:`+req.body.email+
+    `<br>problem:`+req.body.subject+`<br>recieptn:`+req.body.message + `</h3>`// html body
   };
 
   transporter.sendMail(mailOptions,(error,info)=>{
